b09f29476eca9ab1ad93d6f3048e8a2641dc703e
Newmaker <netabcxyz@gmail.com>
2026-04-26 22:23:11 +0800

    delete owner pda

--- wmspda/utils/scan.js
+++
@@ -1,9 +0,0 @@
-export async function scanOne(){
-try{
-const res = await new Promise((resolve,reject)=>{
-uni.scanCode({ onlyFromCamera:true, success:resolve, fail:reject })
-})
-return (res.result||'').trim()
-}catch(e){}
-return ''
-}
